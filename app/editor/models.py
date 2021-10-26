ce38ef2d03cf0501a5334b4713f7e04b6cc19408
Chris Zhu <chriszhu@Chriss-MacBook-Pro.local>
2021-10-25 22:47:48 -0700

    added project metadata

--- app/editor/models.py
+++ app/editor/models.py
@@ -6,6 +6,13 @@ from users.models import User
 class Project(TimestampedUUIDModel):
     name = models.CharField(max_length=256)
     description = models.TextField(default="")
+
+    website = models.CharField(default="", max_length=256)
+    opeansea = models.CharField(default="", max_length=256)
+    twitter = models.CharField(default="", max_length=256)
+    discord = models.CharField(default="", max_length=256)
+    etherscan = models.CharField(default="", max_length=256)
+
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="projects")
     ispublic = models.BooleanField(default=False)
     listed = models.BooleanField(default=False)
