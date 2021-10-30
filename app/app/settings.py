1817240032c8da15df6249b3b9a4817aa30437a9
Chris Zhu <chriszhu@Chriss-MacBook-Pro.local>
2021-10-29 19:16:25 -0700

    added backend.raremints.club

--- app/app/settings.py
+++ app/app/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-&q*4cfq(qgj+avsas@1!-e1j@axwvul5wb!hrwuqigk7ijep8$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', 'nftmaker-backend.onrender.com']
+ALLOWED_HOSTS = ['localhost', 'nftmaker-backend.onrender.com', 'backend.raremints.club']
 
 # Application definition
 
