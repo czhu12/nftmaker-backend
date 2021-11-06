41a24adbf9a75d0e4e959adfbfd7166226b272fc
Chris Zhu <chriszhu@Chriss-MacBook-Pro.local>
2021-11-06 00:50:59 -0400

    remove uniqueness contraint

--- app/users/migrations/0003_user_public_address.py
+++ app/users/migrations/0003_user_public_address.py
@@ -14,6 +14,6 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='user',
             name='public_address',
-            field=base.models.AddressField(blank=True, max_length=256, unique=True),
+            field=base.models.AddressField(blank=True, max_length=256, null=True),
         ),
     ]
