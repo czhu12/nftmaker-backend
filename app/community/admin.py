c985ca1eec2967faf662674828ecc2f722facf06
Chris Zhu <chriszhu@Chriss-MacBook-Pro.local>
2021-12-18 11:43:44 -0800

    added admin features

--- app/community/admin.py
+++ app/community/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
+from community.models import Community, Message, Contract, CommunalCanvas, Pixel
 
-# Register your models here.
+
+class CommunityAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+
+class CommunalCanvasAdmin(admin.ModelAdmin):
+    list_display = ('modified',)
+
+
+class PixelAdmin(admin.ModelAdmin):
+    list_display = ('token_identifier', 'modified')
+
+
+class ContractAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ('token_identifier', 'body')
+
+
+admin.site.register(Community, CommunityAdmin)
+admin.site.register(CommunalCanvas, CommunalCanvasAdmin)
+admin.site.register(Pixel, PixelAdmin)
+admin.site.register(Message, MessageAdmin)
+admin.site.register(Contract, ContractAdmin)
--- app/users/admin.py
+++ app/users/admin.py
@@ -1,25 +1,8 @@
 from django.contrib import admin
-from editor.models import Group, Asset, Layer, Project
 
+from users.models import User
 
-# Register your models here.
-class GroupAdmin(admin.ModelAdmin):
-    list_display = ('name',)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'email')
 
-
-class AssetAdmin(admin.ModelAdmin):
-    list_display = ('name',)
-
-
-class LayerAdmin(admin.ModelAdmin):
-    list_display = ('name',)
-
-
-class ProjectAdmin(admin.ModelAdmin):
-    list_display = ('name',)
-
-
-admin.site.register(Group, GroupAdmin)
-admin.site.register(Asset, AssetAdmin)
-admin.site.register(Layer, LayerAdmin)
-admin.site.register(Project, ProjectAdmin)
+admin.site.register(User, UserAdmin)
